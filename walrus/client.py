18258712ae8264b0178df80d6c1789ea71c671a7
gitlawr <lawrleegle@gmail.com>
2023-08-21 16:53:00 +0800

    refactor: update headers

--- walrus/client.py
+++ walrus/client.py
@@ -11,14 +11,19 @@ class WalrusClient:
         self.api_key = api_key
         self.request_args = kwargs
 
+    def headers(self):
+        """Get default headers."""
+        return {
+            "Authorization": f"Bearer {self.api_key}",
+            "Content-Type": "application/json",
+        }
+
     def list_projects(self):
         """List projects."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-
         response = requests.get(
             url=self.api_url + "/v1/projects",
             params={"perPage": -1},
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -28,14 +33,13 @@ class WalrusClient:
 
     def list_environments(self, project_id: str):
         """List environments."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
         params = {
             "perPage": -1,
         }
         response = requests.get(
             url=self.api_url + f"/v1/projects/{project_id}/environments",
             params=params,
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -45,8 +49,6 @@ class WalrusClient:
 
     def delete_environments(self, project_id: str, ids: List[str]):
         """Delete one or multiple environments."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-
         items = []
         for id in ids:
             items.append({"id": id})
@@ -58,7 +60,7 @@ class WalrusClient:
         body = {"items": items}
         response = requests.delete(
             url=self.api_url + f"/v1/projects/{project_id}/environments",
-            headers=headers,
+            headers=self.headers(),
             json=body,
             **self.request_args,
         )
@@ -69,11 +71,10 @@ class WalrusClient:
 
     def get_environment_graph(self, project_id: str, environment_id: str):
         """Get environment dependency graph."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
         response = requests.get(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/graph",
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -88,13 +89,12 @@ class WalrusClient:
         params = {
             "perPage": -1,
         }
-        headers = {"Authorization": f"Bearer {self.api_key}"}
 
         response = requests.get(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/services",
             params=params,
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -106,12 +106,11 @@ class WalrusClient:
         self, project_id: str, environment_id: str, service_name: str
     ):
         """Get a service by name."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
 
         response = requests.get(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/services/{service_name}",
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -121,12 +120,11 @@ class WalrusClient:
 
     def create_service(self, project_id: str, environment_id: str, data):
         """Create a service in a project and environment."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
 
         response = requests.post(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/services",
-            headers=headers,
+            headers=self.headers(),
             data=data,
             **self.request_args,
         )
@@ -142,12 +140,10 @@ class WalrusClient:
         except json.JSONDecodeError as e:
             raise e
 
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-
         response = requests.put(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/services/{service['id']}/upgrade",
-            headers=headers,
+            headers=self.headers(),
             data=data,
             **self.request_args,
         )
@@ -160,8 +156,6 @@ class WalrusClient:
         self, project_id: str, environment_id: str, ids: List[str]
     ):
         """Delete one or multiple services."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-
         items = []
         for id in ids:
             items.append({"id": id})
@@ -170,7 +164,7 @@ class WalrusClient:
         response = requests.delete(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/services",
-            headers=headers,
+            headers=self.headers(),
             json=body,
             **self.request_args,
         )
@@ -183,12 +177,10 @@ class WalrusClient:
         self, project_id: str, environment_id: str, service_id: str
     ):
         """Get access endpoints of a service."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-
         response = requests.get(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/services/{service_id}/access-endpoints",
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -200,12 +192,10 @@ class WalrusClient:
         self, project_id: str, environment_id: str, service_id: str
     ):
         """List resources of a service."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-
         response = requests.get(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/services/{service_id}/resources",
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -221,12 +211,10 @@ class WalrusClient:
         service_resource_id: str,
     ):
         """Get keys of a service resource."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-
         response = requests.get(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/services/{service_id}/resources/{service_resource_id}/keys",
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -246,13 +234,12 @@ class WalrusClient:
         params = {
             "key": key,
         }
-        headers = {"Authorization": f"Bearer {self.api_key}"}
 
         response = requests.get(
             url=self.api_url
             + f"/v1/projects/{project_id}/environments/{environment_id}/services/{service_id}/resources/{service_resource_id}/log",
             params=params,
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -262,12 +249,10 @@ class WalrusClient:
 
     def list_templates(self):
         """List templates."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-
         response = requests.get(
             url=self.api_url + "/v1/templates",
             params={"perPage": -1},
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
@@ -283,12 +268,10 @@ class WalrusClient:
 
     def get_template_version(self, id: str):
         """Get latest template version given template id."""
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-
         response = requests.get(
             url=self.api_url + f"/v1/templates/{id}/versions",
             params={"templateID": id, "perPage": -1},
-            headers=headers,
+            headers=self.headers(),
             **self.request_args,
         )
         if response.status_code != 200:
