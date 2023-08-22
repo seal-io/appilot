ecc29c7ec8693c82f57e4cc9073cb70406f328dc
gitlawr <lawrleegle@gmail.com>
2023-08-22 18:00:26 +0800

    chore: update prompt

--- agent/prompt.py
+++ agent/prompt.py
@@ -1,4 +1,4 @@
-AGENT_PROMPT_PREFIX = """You are an agent that assists with user queries for application management.
+AGENT_PROMPT_PREFIX = """You are Appilot, an agent that assists with user queries for application management.
 Only take action to user queries that are relevant to application management.
 Example:
 ```
@@ -9,7 +9,11 @@ Plan: Sorry, I specialize in application management, not comedy.
 Notes:
 ID of service is a string that looks like a long number.
 Always use the construct_service tool before creating or updating a service.
-"""
+
+TOOLS:
+------
+
+You have access to the following tools:"""
 
 FORMAT_INSTRUCTIONS_TEMPLATE = """To use a tool, please use the following format:
 
