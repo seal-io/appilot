3857c65a203f5a6e2f350a5b1c59be4c6a8aa6ad
gitlawr <lawrleegle@gmail.com>
2023-08-30 16:25:18 +0800

    chore: add response space

--- i18n/text.py
+++ i18n/text.py
@@ -15,8 +15,8 @@ RESULT:
 system_messages = {
     "welcome": "Appilot: What can I help?",
     "ai_reasoning": "Appilot reasoning:",
-    "response_prefix": "Appilot:",
-    "inform_prefix": "Appilot[inform]:",
+    "response_prefix": "Appilot: ",
+    "inform_prefix": "Appilot[inform]: ",
     "error_occur_message": "An internal error occurred. Enter 'appilot_log' if you want to see the details.",
     "rejected_message": "The action is rejected.",
     "no_error_message": "No error occurred.",
