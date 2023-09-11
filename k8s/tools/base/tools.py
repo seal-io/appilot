851b81a6f5ded04e24da3f8d4672ee78386c1063
gitlawr <lawrleegle@gmail.com>
2023-09-11 17:04:12 +0800

    chore: clean up

--- k8s/tools/base/tools.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-from langchain.agents.tools import BaseTool
-from kubernetes import client
-
-
-class KubernetesTool(BaseTool):
-    """Tool to interacte with Walrus APIs."""
-
-    client: client
--- k8s/tools/manage_template/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tools for managing templates."""
